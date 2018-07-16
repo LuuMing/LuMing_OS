d4b707263ed9c8b484dc8eac717d969520527f5b
LuMing <784315443@qq.com>
2018-07-16 16:32:26 +0800

    Modify setup.s Could print Memory Size Note: ds was 9000 others was 9020 so, if use [addr], it will locat in 0x9000:addr but, the message was in 0x9020:addr

--- boot/setup.s
+++ boot/setup.s
@@ -36,27 +36,39 @@ start:
 	mov ah,#0x03
 	xor bh,bh
 	int 0x10
-	mov cx,#25
+	mov cx,#23
 	mov bx,#0x0007
 	mov bp,#msg
 	mov ax,#0x1301
 	int 0x10
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-! ok, the read went well so we get current cursor position and save it for
-! posterity.
-
-	mov	ax,#INITSEG	! this is done in bootsect already, but...
-	mov	ds,ax
-	mov	ah,#0x03	! read cursor pos
-	xor	bh,bh
-	int	0x10		! save it in known place, con_init fetches
-	mov	[0],dx		! it from 0x90000.
 ! Get memory size (extended mem, kB)
 
 	mov	ah,#0x88
 	int	0x15
 	mov	[2],ax
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!print Memory size
+	mov ah,#0x03
+        xor bh,bh
+        int 0x10
+        mov cx,#17
+        mov bx,#0x0007
+        mov bp,#msg_2
+        mov ax,#0x1301
+        int 0x10
+	call print_hex
+	mov ah,#0x03
+        xor bh,bh
+        int 0x10
+	mov cx,#4
+        mov bx,#0x0007
+        mov bp,#msg_2
+	add bp,#17
+        mov ax,#0x1301
+        int 0x10
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 ! Get video-card data:
 
@@ -86,6 +98,15 @@ start:
 	rep
 	movsb
 
+! ok, the read went well so we get current cursor position and save it for
+! posterity.
+
+	mov	ax,#INITSEG	! this is done in bootsect already, but...
+	mov	ds,ax
+	mov	ah,#0x03	! read cursor pos
+	xor	bh,bh
+	int	0x10		! save it in known place, con_init fetches
+	mov	[0],dx		! it from 0x90000.
 ! Get hd1 data
 
 	mov	ax,#0x0000
@@ -234,11 +255,31 @@ gdt_48:
 	.word	0x800		! gdt limit=2048, 256 GDT entries
 	.word	512+gdt,0x9	! gdt base = 0X9xxxx
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+print_hex:
+mov    cx,#4         ! 4个十六进制数字
+mov    dx,ax     
+print_digit:
+rol    dx,#4        ! 循环以使低4比特用上 !! 取dx的高4比特移到低4比特处。
+mov    ax,#0xe0f     ! ah = 请求的功能值，al = 半字节(4个比特)掩码。
+and    al,dl        ! 取dl的低4比特值。
+add    al,#0x30     ! 给al数字加上十六进制0x30
+cmp    al,#0x3a
+jl    outp        !是一个不大于十的数字
+    add    al,#0x07      !是a～f，要多加7
+outp: 
+int    0x10
+loop    print_digit
+ret
+
 msg:
 	.byte 13,10
 	.ascii "Now we are in SETUP"
-	.byte 13,10,13,10
-
+	.byte 13,10
+msg_2:
+	.byte 13,10
+	.ascii "Memory Size is "
+	.ascii "KB"
+	.byte 13,10
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 .text
 endtext:
