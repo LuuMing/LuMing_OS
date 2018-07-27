d684c25f2c270415e20f74bb00bdfc9e6ca6e136
LuMing <784315443@qq.com>
2018-07-27 21:02:26 +0800

    Complete first version of whoami add two system_call iam: to write some string to kernel space whoami: transimit kernel space's data into user space and print ★ core function is get_fs_byte() and put_fs_byte() fix bug in lib

--- kernel/who.c
+++ kernel/who.c
@@ -1,7 +1,29 @@
-int sys_whoami()
+#include<asm/segment.h>
+#include<errno.h>
+extern int errno;
+char myname[23];
+int sys_whoami(char * name, unsigned int size)
 {
+	unsigned int i = 0;
+	for( i = 0; myname[i] != 0; i++)
+		put_fs_byte(myname[i],&name[i]);
+	put_fs_byte(0,&name[i]);
+	if( i > size)
+	{
+		errno = EINVAL;
+		return -1;
+	}
+	else
+		return i;
 }
-int sys_iam()
+int sys_iam(const char * name)
 {
-	printk("I am luuMing");
+	int i = 0;
+	for( i = 0; i < 23; i++)
+	{
+		myname[i] = get_fs_byte(&name[i]);
+		if(myname[i] == 0)
+			break;
+	}
+	printk("%s\n",myname);
 }
--- lib/whoami.c
+++ lib/whoami.c
@@ -1,3 +1,3 @@
 #define __LIBRARY__
 #include<unistd.h>
-_syscall2(int,whoami,char,name,unsigned int,size)
+_syscall2(int,whoami,char*,name,unsigned int,size)
