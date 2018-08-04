3460fd45f2a0f7cd6e739406f9b0df228c3c69ba
LuMing <784315443@qq.com>
2018-08-04 21:12:52 +0800

    Experimental modification add to print some message when create an process

--- kernel/fork.c
+++ kernel/fork.c
@@ -82,6 +82,9 @@ int copy_process(int nr,long ebp,long edi,long esi,long gs,long none,
 	*p = *current;	/* NOTE! this doesn't copy the supervisor stack */
 	p->state = TASK_UNINTERRUPTIBLE;
 	p->pid = last_pid;
+	/////////////////////////////////////////
+	fprintk(3, "%ld\t%c\t%ld\n", p->pid, 'N', jiffies);
+	/////////////////////////////////////////
 	p->father = current->pid;
 	p->counter = p->priority;
 	p->signal = 0;
@@ -130,6 +133,9 @@ int copy_process(int nr,long ebp,long edi,long esi,long gs,long none,
 	set_tss_desc(gdt+(nr<<1)+FIRST_TSS_ENTRY,&(p->tss));
 	set_ldt_desc(gdt+(nr<<1)+FIRST_LDT_ENTRY,&(p->ldt));
 	p->state = TASK_RUNNING;	/* do this last, just in case */
+	///////////////////////////////////
+	fprintk(3, "%ld\t%c\t%ld\n", p->pid, 'J', jiffies);
+	///////////////////////////////////
 	return last_pid;
 }
 
